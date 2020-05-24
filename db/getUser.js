7cc903141539a433387a01885310d1ddd327e2f8
omar <omar.csse@gmail.com>
2020-05-25 03:58:36 +1000

    refactor

--- db/getUser.js
+++ db/getUser.js
@@ -3,14 +3,8 @@ const StocksDB = require('../config/db')
 
 const getUser = async (email) => {
     const query = 'SELECT * FROM users WHERE email = ?'
-    console.log(email)
-    
-    try {
-        const [rows, _] = await StocksDB.db.execute(query, [email])
-        return rows
-    } catch (error) {
-        console.log(error)
-    }
+    const [rows, _] = await StocksDB.db.execute(query, [email])
+    return rows  
 }
 
 
