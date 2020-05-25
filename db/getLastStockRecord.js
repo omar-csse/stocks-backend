0f16617c86dbf84f50080b7f1e91c1fee9416033
omar <omar.csse@gmail.com>
2020-05-26 02:28:16 +1000

    edit symbol route

--- db/getLastStockRecord.js
+++ db/getLastStockRecord.js
@@ -4,6 +4,7 @@ const StocksDB = require('../config/db')
 const getLastStockRecord = async (symbol) => {
     const query = 'SELECT * FROM stocks WHERE symbol = ? LIMIT 1;'
     const [rows, _] = await StocksDB.db.execute(query, [`${symbol}`])
+    console.log(rows)
     return rows
 }
 
--- routes/stocks/stocks.js
+++ routes/stocks/stocks.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
 const symbolsRouter = require('./symbols')
 const authedRouter = require('./authed')
-
+const getLastStockRecord = require('../../db/getLastStockRecord')
+const err = require('../../models/errors')
 
 router.use('/symbols', symbolsRouter)
 router.use('/authed', authedRouter)
+
+
+router.get('/:symbol' , async (req, res) => {
+    try {
+
+        if (Object.keys(req.query).length > 0) {
+            res.status(400).send(err.err_400_symbol)
+            return;
+        } 
+
+        rows = await getLastStockRecord(req.params.symbol)
+        rows.length > 0 ? res.send(rows) : res.status(404).send(err.err_404_symbol)
+
+    } catch (error) {
+        console.log(`💽  StocksDB: ${error.sqlMessage || 'Error'}`)
+        res.status(502).send(err.err_502_db)
+    }
+})
 
 
 module.exports = router;
