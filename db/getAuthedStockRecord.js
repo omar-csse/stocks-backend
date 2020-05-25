80de607b07e9207f9b50b926ea7c6ab438350462
omar <omar.csse@gmail.com>
2020-05-26 02:52:14 +1000

    use mysql.format to prevent sql injection

--- db/getAuthedStockRecord.js
+++ db/getAuthedStockRecord.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2/promise');
 const StocksDB = require('../config/db')
 const h = require('../models/helper')
 
@@ -6,7 +7,7 @@ const getAuthedStockRecord = async (symbol, from, to) => {
     const query = `SELECT * FROM stocks WHERE symbol = ? AND timestamp BETWEEN from_unixtime(?) AND from_unixtime(?)`
     const pFrom = h.tstamp(from) || (1 / 1000)
     const pTo = h.tstamp(to) || (Date.now() / 1000)
-    const [rows, _] = await StocksDB.db.execute(query, [symbol, pFrom, pTo])
+    const [rows, _] = await StocksDB.db.query(mysql.format(query, [symbol, pFrom, pTo]))
     return rows
 }
 
--- db/getLastStockRecord.js
+++ db/getLastStockRecord.js
@@ -1,10 +1,10 @@
+const mysql = require('mysql2/promise');
 const StocksDB = require('../config/db')
 
 
 const getLastStockRecord = async (symbol) => {
     const query = 'SELECT * FROM stocks WHERE symbol = ? LIMIT 1;'
-    const [rows, _] = await StocksDB.db.execute(query, [`${symbol}`])
-    console.log(rows)
+    const [rows, _] = await StocksDB.db.query(mysql.format(query, [symbol]))
     return rows
 }
 
