b505e2e3ab2d321a5846b6ef94936022dff24b6e
omar <omar.csse@gmail.com>
2020-06-02 23:42:08 +1000

    edit swagger url

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended: true}));
 
 
 /* Routes */
-app.use('/', apiRouter)
+app.use('/api', apiRouter)
 app.use('/stocks', stocksRouter)
 app.use('/user', userRouter)
 
