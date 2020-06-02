75fff7c89da500d38ee63d45df7d8953372705ac
omar <omar.csse@gmail.com>
2020-06-02 23:09:36 +1000

    change swagger route to /

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended: true}));
 
 
 /* Routes */
-app.use('/api', apiRouter)
+app.use('/', apiRouter)
 app.use('/stocks', stocksRouter)
 app.use('/user', userRouter)
 
