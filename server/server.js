d17fef25591e897dd2b08b5f0d0dc511a106a653
omar <omar.csse@gmail.com>
2020-06-04 05:22:57 +1000

    edit handling 404

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use('/stocks', stocksRouter)
 app.use('/user', userRouter)
 
 /* Hanlde 404 routes */
-app.get('*', (req, res) => res.status(404).send(err.err_404_page))
+app.get('/', (req, res) => res.status(404).send(err.err_404_page))
 
 
 const main = async () => {
